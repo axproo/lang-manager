3c9e85f398b849d9e8923206b71f42d3bb72d33b
Christian DJOMOU <developper@axproo.com>
2025-11-26 14:07:40 +0100

    update lang-manager

--- src/dictionaries/en-en.php
+++ src/dictionaries/en-en.php
@@ -4,9 +4,5 @@ return [
     'login.success' => 'Login successful',
     'login.unauthorized' => 'Unauthorized access',
     'logout.success' => 'Logout successful',
-    'failed.email.required' => 'Email require',
-    'failed.email.valid_email' => '',
-    'failed.email.incorrect' => '',
-    'failed.password.required' => 'Password require',
-    'failed.password.length' => ''
+    'failed.email.required' => 'Email require'
 ];
