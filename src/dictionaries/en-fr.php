09b4210c3845b64c254bb0b7c455b7f3c017eaba
Christian DJOMOU <developper@axproo.com>
2025-11-26 12:08:04 +0100

    update 1.0.1 stable

--- src/dictionaries/en-fr.php
+++ src/dictionaries/en-fr.php
@@ -5,8 +5,4 @@ return [
     'login.unauthorized' => 'Accès non autorisé',
     'logout.success' => 'Déconnexion réussie',
     'failed.email.required' => 'Email obligatoire',
-    'failed.email.valid_email' => '',
-    'failed.email.incorrect' => '',
-    'failed.password.required' => '',
-    'failed.password.length' => ''
 ];
